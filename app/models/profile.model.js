f8af9c9fa114ab7d366add07fe5ae5994f74930c
Matthew Hyrka <mhyrka@gmail.com>
2018-06-26 14:39:53 -0600

    data seeded

--- app/models/profile.model.js
+++ app/models/profile.model.js
@@ -1,31 +1,29 @@
 const mongoose = require('mongoose');
 
-const Profiles = mongoose.Schema({name: String})
+// const Profiles = mongoose.Schema({name: String})
 
-// const Profiles = mongoose.Schema({
-//     name: String,
-//     age: Number,
-//     location: {
-//       "timestamp": Number,
-//       "coords": {
-//         "accuracy": Number,
-//         "altitude": Number,
-//         "altitudeAccuracy": Number,
-//         "heading": Number,
-//         "latitude": Number,
-//         "longitude": Number,
-//         "speed": Number }
-//       },
-//     image: String,
-//     friends: [String],
-//     active: Boolean,
-//     messages: [
-//       {
-//          user: String,
-//          message: String
-//       }
-//    ]
-//
-// });
+const Profiles = mongoose.Schema({
+    name: String,
+    age: Number,
+
+    location: {
+      "timestamp": Number,
+      "coords": {
+        "accuracy": Number,
+        "altitude": Number,
+        "altitudeAccuracy": Number,
+        "heading": Number,
+        "latitude": Number,
+        "longitude": Number,
+        "speed": Number
+        }
+    },
+
+    image: Object,
+    friends: [String],
+    active: Boolean,
+    messages: [ { user: String, message: String } ]
+
+});
 
 module.exports = mongoose.model('Profiles', Profiles);
--- seeds/seeds.js
+++ seeds/seeds.js
@@ -1,115 +1,112 @@
-const {mongoose, db} = require('../config/database.config.js');
-const Profiles = require('../app/models/profile.model.js')
-
-// const Profiles = mongoose.model('Profiles');
+const { mongoose, db } = require("../config/database.config.js")
+const Profiles = require("../app/models/profile.model.js")
 
 const data = {
-  profiles: [{name:"mark"}, {name: ";LJASDFJLASDFLJK "}]
-  // "profiles": [{
-  //     "name": "Muffins",
-  //     "age": 1.5,
-  //     "location": {
-  //       "timestamp": 1529873085,
-  //       "coords": {
-  //         "accuracy": 5,
-  //         "altitude": 0,
-  //         "altitudeAccuracy": -1,
-  //         "heading": -1,
-  //         "latitude": 39.75321812,
-  //         "longitude": -104.78614374,
-  //         "speed": -1 },
-  //     "image": "https://s3.us-east-2.amazonaws.com/diggity-image-hosting/adorable-animal-bull-160846.jpg",
-  //     "friends": [],
-  //     "active": true,
-  //     "messages": [
-  //       {
-  //          "user": "",
-  //          "message": ""
-  //       }
-  //    ]
-  //
-  //  }
-  // },
-  // {
-  //     "name": "Chloe",
-  //     "age": .7,
-  //     "location": {
-  //       "timestamp": 1529527485,
-  //       "coords": {
-  //         "accuracy": 5,
-  //         "altitude": 0,
-  //         "altitudeAccuracy": -1,
-  //         "heading": -1,
-  //         "latitude": 39.68146399,
-  //         "longitude": -104.99532397,
-  //         "speed": -1 },
-  //     "image": "https://s3.us-east-2.amazonaws.com/diggity-image-hosting/animal-collar-dog-8700.jpg",
-  //     "friends": [],
-  //     "active": true,
-  //     "messages": [
-  //       {
-  //          "user": "",
-  //          "message": ""
-  //       }
-  //    ]
-  //
-  //  }
-  // },
-  // {
-  //     "name": "Roxanne",
-  //     "age": 3,
-  //     "location": {
-  //       "timestamp": 1529441085,
-  //       "coords": {
-  //         "accuracy": 5,
-  //         "altitude": 0,
-  //         "altitudeAccuracy": -1,
-  //         "heading": -1,
-  //         "latitude": 39.66232937,
-  //         "longitude": -104.89177581,
-  //         "speed": -1 },
-  //     "image": "https://s3.us-east-2.amazonaws.com/diggity-image-hosting/animal-corgi-dog-58997.jpg",
-  //     "friends": [],
-  //     "active": true,
-  //     "messages": [
-  //       {
-  //          "user": "",
-  //          "message": ""
-  //       }
-  //    ]
-  //
-  //  }
-  // },
-  // {
-  //     "name": "John Rambo",
-  //     "age": 2,
-  //     "location": {
-  //       "timestamp": 1528663485,
-  //       "coords": {
-  //         "accuracy": 5,
-  //         "altitude": 0,
-  //         "altitudeAccuracy": -1,
-  //         "heading": -1,
-  //         "latitude": 39.88633666,
-  //         "longitude": -105.15221538,
-  //         "speed": -1 },
-  //     "image": "https://s3.us-east-2.amazonaws.com/diggity-image-hosting/animal-dog-golden-retriever-9080.jpg",
-  //     "friends": [],
-  //     "active": true,
-  //     "messages": [
-  //       {
-  //          "user": "",
-  //          "message": ""
-  //       }
-  //    ]
-  //
-  //  }
-  // }]
-
+  profiles: [
+    {
+      name: "Muffins",
+      age: 1.5,
+      location: {
+        timestamp: 1529873085,
+        coords: {
+          accuracy: 5,
+          altitude: 0,
+          altitudeAccuracy: -1,
+          heading: -1,
+          latitude: 39.75321812,
+          longitude: -104.78614374,
+          speed: -1
+        }
+      },
+      image: "https://s3.us-east-2.amazonaws.com/diggity-image-hosting/adorable-animal-bull-160846.jpg",
+      friends: [],
+      active: true,
+      messages: [
+        {
+          user: "",
+          message: ""
+        }
+      ]
+    },
+    {
+      name: "Chloe",
+      age: 0.7,
+      location: {
+        timestamp: 1529527485,
+        coords: {
+          accuracy: 5,
+          altitude: 0,
+          altitudeAccuracy: -1,
+          heading: -1,
+          latitude: 39.68146399,
+          longitude: -104.99532397,
+          speed: -1
+        }
+      },
+      image: "https://s3.us-east-2.amazonaws.com/diggity-image-hosting/animal-collar-dog-8700.jpg",
+      friends: [],
+      active: true,
+      messages: [
+        {
+          user: "",
+          message: ""
+        }
+      ]
+    },
+    {
+      name: "Roxanne",
+      age: 3,
+      location: {
+        timestamp: 1529441085,
+        coords: {
+          accuracy: 5,
+          altitude: 0,
+          altitudeAccuracy: -1,
+          heading: -1,
+          latitude: 39.66232937,
+          longitude: -104.89177581,
+          speed: -1
+        }
+      },
+      image: "https://s3.us-east-2.amazonaws.com/diggity-image-hosting/animal-corgi-dog-58997.jpg",
+      friends: [],
+      active: true,
+      messages: [
+        {
+          user: "",
+          message: ""
+        }
+      ]
+    },
+    {
+      name: "John Rambo",
+      age: 2,
+      location: {
+        timestamp: 1528663485,
+        coords: {
+          accuracy: 5,
+          altitude: 0,
+          altitudeAccuracy: -1,
+          heading: -1,
+          latitude: 39.88633666,
+          longitude: -105.15221538,
+          speed: -1
+        }
+      },
+      image: "https://s3.us-east-2.amazonaws.com/diggity-image-hosting/animal-dog-golden-retriever-9080.jpg",
+      friends: [],
+      active: true,
+      messages: [
+        {
+          user: "",
+          message: ""
+        }
+      ]
+    }
+  ]
 }
 
-Profiles.remove({})
-.then(() => {
+Profiles.remove({}).then(() => {
   Profiles.insertMany(data.profiles)
 })
 
